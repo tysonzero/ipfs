d491c275036a91f21525d98b130f6b134a967bfa
Daniel Smith <danielspencersmith@gmail.com>
2021-11-28 03:05:24 -0800

    Add ghcjs setup workaround

--- multihash-cryptonite/Setup.hs
+++ multihash-cryptonite/Setup.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE CPP #-}
+
 module Main where
 
+#ifdef ghcjs_HOST_OS
+import Distribution.Simple (defaultMain)
+
+main :: IO ()
+main = defaultMain
+#else
 import Distribution.Extra.Doctest (defaultMainWithDoctests)
 
 main :: IO ()
 main = defaultMainWithDoctests "doctests"
+#endif
